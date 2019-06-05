667fe5ecdc975d391942512ce570d7492e49fd30
jayashrri <balaji.jayashrri@gmail.com>
2019-06-05 17:49:14 +0530

    Connect to Server

---
+++ cernserver.php
@@ -0,0 +1,13 @@
+#!/usr/bin/php
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+$conn = new mysqli($servername,$username,$password);
+if($conn->connect_error){
+    die("Connection Failed: " . $conn->connect_error);
+}
+echo "Connected Successfully";
+?>
